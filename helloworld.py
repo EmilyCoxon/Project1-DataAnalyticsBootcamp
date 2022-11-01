6010e3cde4a7fff53fc5b2debb6a3a2d199fca55
p.anshuman1 <p.anshuman1@gmail.com>
2022-11-01 20:02:14 +0800

    thetbe

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 print("Hello world. This is my first file for Project 1")
 print("Another line")
+=======
+print("Hello World. This is first file push")
+>>>>>>> 44cb8ae (added helloworld.py file that print a generic message)
